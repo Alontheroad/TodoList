7a5ed6d5a108181e3ae5841a386d7c507a856fe6
Alontheroad <aloys.ratinet@me.com>
2018-09-12 15:55:23 -0400

    Data persisted using UserDefaults method.

--- TodoList/ToDoListViewController.swift
+++ TodoList/ToDoListViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["Finir demande de Visa", "Envoyer email à Pascal", "Faire du sport aujourd'hui"]
+    var itemArray = ["Finir demande de Visa", "Envoyer email à Pascal", "Faire du sport aujourd'hui"]
+    
+    // Pour sauvegarder nos données même si on éteint l'application
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Pour Afficher les données sauvegardées
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK - Tableview Datasource Methods
@@ -39,17 +47,39 @@ class ToDoListViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-      
-//        itemArray[indexPath.row]
-        
-        
         
      // supprime l'effet de couleur (gris) sur la ligne sélectionnée après sélection.
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
 
-
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Ajouter une nouvelle tâche", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ajouter une tâche", style: .default) { (action) in
+            // what will happen once the user clicks the Add item button on our UIAlert
+            self.itemArray.append(textField.text!)
+            
+            // Accès à nos données si l'iphone est éteint
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            // met à jour la tableView pour afficher notre nouvel item
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Créer une nouvelle tâche"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     
     
     
