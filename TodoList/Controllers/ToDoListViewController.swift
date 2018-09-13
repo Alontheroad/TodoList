df8234e7c03083a7387a25e47f8e8bb82abc8563
Alontheroad <aloys.ratinet@me.com>
2018-09-13 12:56:32 -0400

    Convert to MVC design pattern

--- TodoList/Controllers/ToDoListViewController.swift
+++ TodoList/Controllers/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Finir demande de Visa", "Envoyer email à Pascal", "Faire du sport aujourd'hui"]
+    var itemArray = [Item]()
     
     // Pour sauvegarder nos données même si on éteint l'application
     let defaults = UserDefaults.standard
@@ -18,8 +18,22 @@ class ToDoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let newItem = Item()
+        newItem.title = "Finir demande de visa"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Appeler Jason ce soir"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Progresser sur Swift"
+        itemArray.append(newItem3)
+        
+
+        
         // Pour Afficher les données sauvegardées
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
     }
@@ -31,8 +45,26 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        
+        // Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        // cell.accessoryType = item.done (== true) ? .checkmark : .none
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
         return cell
     }
     
@@ -40,13 +72,21 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        
+        
+        //Supprimer l'effet checkmark redondant du fait du reusable
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        
+        tableView.reloadData()
+        
         //Ajouter un accessory : "checkmark"
         //Supprimer le checkmark s'il existe avec un "none"
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
      // supprime l'effet de couleur (gris) sur la ligne sélectionnée après sélection.
         tableView.deselectRow(at: indexPath, animated: true)
@@ -61,7 +101,10 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Ajouter une tâche", style: .default) { (action) in
             // what will happen once the user clicks the Add item button on our UIAlert
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             // Accès à nos données si l'iphone est éteint
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
